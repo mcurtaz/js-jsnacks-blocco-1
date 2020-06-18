3a27ad91baf317de703bf9d916e97d15d867ea4b
Michele Curtaz <mcurtaz@gmail.com>
2020-06-18 17:40:12 +0200

    Commentata animazione

--- js-snack-6/JSSnack6.js
+++ js-snack-6/JSSnack6.js
@@ -1,5 +1,6 @@
 // JS Snack 6
 // Nella pagina c'è un bottone#lanciaMoneta. Al click su un bottone, il programma genera un numero casuale (tra 0 e 9). Se il numero è pari mostra un img con la testa della moneta. Se è dispari mostra un'altra img con la croce della moneta.
+
 var testa = document.getElementById("testa");
 var croce = document.getElementById("croce");
 
@@ -13,17 +14,19 @@ document.getElementById("lancia").addEventListener("click", function(){
   console.log(num);
 
   testa.classList.remove("display-none");
-  testa.classList.remove("roll");
   croce.classList.remove("display-none");
-  croce.classList.remove("roll");
+  // testa.classList.remove("roll");
+  // croce.classList.remove("roll");
+  
 
   // se è pari metto classe display none a croce altrimenti la metto a Testa
   if (num % 2){
     croce.classList.add("display-none");
-    testa.classList.add("roll");
+    // se assegno la classe roll la moneta esegue l'animazione ma per qualche motivo soltanto quando cambia l'immagine. Per esempio se esce due volte pari l'animazione non si attiva
+    // testa.classList.add("roll");
   } else{
     testa.classList.add("display-none");
-    croce.classList.add("roll");
+    // croce.classList.add("roll");
   }
 
 });
